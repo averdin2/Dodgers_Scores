dc4ffcefd49e330844f17dc9351590d3c4d71b37
averdin2 <alexverdin01@gmail.com>
2020-02-19 14:43:41 -0800

    Updates

---
+++ precache-manifest.e9ee37fcc6c30f88a22cc0e198e66364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a75841660148d88b20616679ddd4972a",
+    "url": "/Dodgers_Scores/static/media/Dodger Stadium Field.a7584166.jpeg"
+  },
+  {
+    "revision": "e781e392ee23198b400e",
+    "url": "/Dodgers_Scores/static/js/runtime~main.51e2b673.js"
+  },
+  {
+    "revision": "3e5bb2c66713599dfd5d",
+    "url": "/Dodgers_Scores/static/js/main.10198c68.chunk.js"
+  },
+  {
+    "revision": "3260c58e24b9cf28544b",
+    "url": "/Dodgers_Scores/static/js/2.43c0e60f.chunk.js"
+  },
+  {
+    "revision": "3e5bb2c66713599dfd5d",
+    "url": "/Dodgers_Scores/static/css/main.3446412c.chunk.css"
+  },
+  {
+    "revision": "312a576426aa0e133e93a364848b4d72",
+    "url": "/Dodgers_Scores/index.html"
+  }
+];
